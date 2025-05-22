6f6a68cac7dd75b442214a4cac04e8e3476f7330
Alexgruden <143414700+Alexgruden@users.noreply.github.com>
2025-05-22 18:29:43 -0400

    Uploaded project files

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Optimizing Solar Tracking using AI
+
+The main purpose of this project is to fundamentally design a solar tracking system to improve the efficiency of the solar panel systems that may have solar tracking capabilities. This study demonstrates how we can fundamentally improve the accuracy of our solar tracking capabiities while observing the charactersistics of different AI NN types.
+
+Observed AI NN Types:
+
+Artificial Neural Network (ANN)
+Kolmogorov-Arnold Networks (KAN)
+Possible AI NN Types:
+
+Recurrent Neural Networks (RNN)
+Generative Averserial Networks (GAN)
+Radial Basis Function (RBF)
+Boltzaman Machine (BM)
+Modular Neural Network (Mod-NN)
+"""
+
+# Libraries for data science & analytics
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Libraries for solar stuff
+# from pysolar.solar import *
+import datetime
+import numpy as np
+
+%matplotlib inline
